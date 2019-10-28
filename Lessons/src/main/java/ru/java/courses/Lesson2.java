db45a02c6527e19f03cf1bc8e44c64db2ca0b8ae
DMITRIYTSAREV <57015831+dmitriytsarev@users.noreply.github.com>
2019-10-28 21:34:38 +0300

    Homework 1

--- Lessons/src/main/java/ru/java/courses/Lesson2.java
+++ Lessons/src/main/java/ru/java/courses/Lesson2.java
@@ -1,4 +1,6 @@
 package ru.java.courses;
+import java.util.Scanner;
+import java.lang.String;
 
 class Lesson2 {
 
@@ -7,13 +9,54 @@ class Lesson2 {
      * Подсказка: квадратный корень вычисляется функцией Math.sqrt()
      *
      * @param x на текущем этапе входное значение будет больше нуля
-     *
      * @return возвращаем только целую часть от полученного результата,
      * хвост отбрасываем без откругления
      */
-    static int formula(int x) {
+    public static void main(String[] args) {
+        int y = 0;
+
+        System.out.println("Введите X");
+        Scanner x = new Scanner(System.in);
+        formula(y, x.nextInt());
+
+
+        System.out.println("введите ФИО");
+        String str;
+        Scanner strlength = new Scanner(System.in);
+        str = strlength.nextLine();
+        char k[] = new char[3];
+
+
+        byte s = 0;
+
+        k[0] = str.charAt(0);
+        s++;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                k[s] = str.charAt(i + 1);
+                s++;
+            }
+
+
+        }
+
+        char F = Character.toUpperCase(k[0]);
+        char I = Character.toUpperCase(k[1]);
+        char O = Character.toUpperCase(k[2]);
+        char pr = '.';
+        String FIO = new StringBuilder().append(F).append(pr).append(I).append(pr).append(O).append(pr).toString();
+        System.out.println(FIO);
+
+
+    }
+
+    static double formula(double y, int x) {
+        y = Math.sqrt(13 * x + (13 / x));
+        y = (int) y;
+        System.out.println(y);
         return 0;
     }
+}
 
     /**
      * Нужно привести строку с полным именем к инициалам.
@@ -28,8 +71,6 @@ class Lesson2 {
      *
      * @return возвращаем инициалы в верхнем регистре разделенные точкой, как в примере выше
      */
-    static String initials(String fullName) {
-        return null;
-    }
 
-}
+
+
